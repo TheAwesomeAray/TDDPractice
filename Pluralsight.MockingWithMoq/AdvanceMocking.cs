5419964ec2a4f0e3d99a5c014fbd7941c8d90843
TheAwesomeARay <andrewray4@gmail.com>
2019-01-13 16:34:39 -0600

    Completed the moqing course

---
+++ Pluralsight.MockingWithMoq/AdvanceMocking.cs
@@ -0,0 +1,125 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq.Protected;
+
+namespace Pluralsight.MockingWithMoq
+{
+    public class AdvanceMocking
+    {
+        //Strict and Loose Mocking
+        [Fact]
+        public void StrictMockExample()
+        {
+            var mockrepo = new Mock<ICustomerRepository>(MockBehavior.Strict);
+            mockrepo.Setup(x => x.Save(It.IsAny<Customer>()));
+            var customerService = new CustomerService(mockrepo.Object);
+
+            customerService.Create(new CustomerToCreateDto());
+
+            //Will fail, because items used are not setup
+            mockrepo.Verify();
+        }
+
+        [Fact]
+        public void BaseClassImplementationsAKAPartialMocks()
+        {
+            //Often used with Web/Html controls in System.Web
+            var mockNameFormatter = new Mock<CustomerNameFormatter>();
+
+            mockNameFormatter.Object.From(new Customer(new CustomerToCreateDto()));
+
+            mockNameFormatter.Verify(x => x.ParseBadWordsFrom(It.IsAny<string>()), Times.Exactly(2));
+        }
+
+        [Fact]
+        public void RecursiveMocking()
+        {
+            var mockCustomerRepo = new Mock<ICustomerRepository>() { DefaultValue = DefaultValue.Mock };
+            //Default Value of Mock sets the Mock to try and Mock all its members
+
+            var customerService = new CustomerService(mockCustomerRepo.Object);
+            var mailingAddressRepo = mockCustomerRepo.Object.MailingRepository;
+            var mock = Mock.Get(mailingAddressRepo);
+
+            customerService.RecursiveMockingTest();
+
+            mock.Verify(x => x.From(It.IsAny<CustomerToCreateDto>()));
+        }
+
+        [Fact]
+        public void MockRepositoryCreation()
+        {
+            //I can create a mockRepository and use it to create future mocks, defining the rules they should follow. I can
+            //define their behavior in one place
+            var mockFactory = new MockRepository(MockBehavior.Loose)
+            { DefaultValue = DefaultValue.Mock };
+            var mockCustomerRepository = mockFactory.Create<ICustomerRepository>();
+
+            //I can also verify all the mocks created by the factory, avoiding a chain of verify calls
+            mockFactory.Verify();
+        }
+
+        [Fact]
+        public void ProtectedMembersExample()
+        {
+            var mockNameFormatter = new Mock<CustomerNameFormatterProtectedExample>();
+
+            //Caveats - No intellisense
+            mockNameFormatter.Protected()
+                 .Setup<string>("ParseBadWordsFrom", ItExpr.IsAny<string>())
+                 .Returns("asdf")
+                 .Verifiable();
+            mockNameFormatter.Object.From(new Customer(new CustomerToCreateDto()));
+            
+            mockNameFormatter.Verify();
+        }
+
+        //Base class implementations
+        //Recursive mocking
+        //Centralized Mock Creation
+        //Protected members
+
+
+    }
+
+    public class CustomerNameFormatter : BaseFormatter
+    {
+        public string From(Customer customer)
+        {
+            var firstName = ParseBadWordsFrom(customer.FirstName);
+            var lastName = ParseBadWordsFrom(customer.LastName);
+
+            return string.Format("{0}, {1}", lastName, firstName);
+        }
+    }
+
+    public class CustomerNameFormatterProtectedExample : BaseFormatterProtectedExample
+    {
+        public string From(Customer customer)
+        {
+            var firstName = ParseBadWordsFrom(customer.FirstName);
+            var lastName = ParseBadWordsFrom(customer.LastName);
+
+            return string.Format("{0}, {1}", lastName, firstName);
+        }
+    }
+
+    public class BaseFormatterProtectedExample
+    {
+        protected virtual string ParseBadWordsFrom(string source)
+        {
+            return "";
+        }
+    }
+
+    public class BaseFormatter
+    {
+        public virtual string ParseBadWordsFrom(string source)
+        {
+            return "";
+        }
+    }
+}
--- Pluralsight.MockingWithMoq/MoqTests.cs
+++ Pluralsight.MockingWithMoq/MoqTests.cs
@@ -86,16 +86,108 @@ namespace Pluralsight.MockingWithMoq
 
             mockIdFactory.Verify(x => x.Create(), Times.AtLeastOnce());
         }
+
+        [Fact]
+        public void FullNameShouldBeCreatedFromFirstAndLastName()
+        {
+            var dto = new CustomerToCreateDto() { FirstName = "Bob", LastName = "Builder" };
+            var mockRepository = new Mock<ICustomerRepository>();
+            var mockFullNameBuilder = new Mock<ICustomerFullNameBuilder>();
+            var customerService = new CustomerService(mockRepository.Object, mockFullNameBuilder.Object);
+
+            customerService.CustomerFullNameBuilderTest(dto);
+
+            //Ensure correct values are being passed into a method
+            mockFullNameBuilder.Verify(x => x.From(
+                It.Is<string>(fn => fn.Equals(dto.FirstName, StringComparison.InvariantCultureIgnoreCase)),
+                It.Is<string>(fn => fn.Equals(dto.LastName, StringComparison.InvariantCultureIgnoreCase))));
+        }
+
+        [Fact]
+        public void TheLocalTimeZoneShouldBeSet()
+        {
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            var customerService = new CustomerService(mockCustomerRepository.Object);
+
+            customerService.Create(new CustomerToCreateDto());
+
+            //Test is a property setter was called
+            mockCustomerRepository.VerifySet(x => x.LocalTimeZone = It.IsAny<string>());
+        }
+
+        [Fact]
+        public void TheLocalTimeZoneShouldBeUsed()
+        {
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            mockCustomerRepository.Setup(x => x.LocalTimeZone).Returns("Test");
+            var customerService = new CustomerService(mockCustomerRepository.Object);
+
+            customerService.Create(new CustomerToCreateDto());
+
+            //Test is a property setter was called
+            mockCustomerRepository.VerifyGet(x => x.LocalTimeZone);
+        }
+
+        [Fact]
+        public void MockingWithLawOfDemeterViolations()
+        {
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            mockCustomerRepository.Setup(x => x.Customer.MailingAddress.Address).Returns("Test");
+            var customerService = new CustomerService(mockCustomerRepository.Object);
+
+            customerService.Create(new CustomerToCreateDto());
+
+            //Test is a property setter was called
+            mockCustomerRepository.VerifyGet(x => x.LocalTimeZone);
+        }
+
+        [Fact]
+        public void StubbingProperties()
+        {
+            //I want to set values, not just have them return a fixed value
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            //There is an overload that lets us set the initial value as well
+            mockCustomerRepository.SetupProperty(x => x.LocalTimeZone, "Test1");
+            //Can also set up all properties
+            //mockCustomerRepository.SetupAllProperties();
+            var customerService = new CustomerService(mockCustomerRepository.Object);
+
+            customerService.Create(new CustomerToCreateDto());
+
+            //Test is a property setter was called
+            mockCustomerRepository.VerifyGet(x => x.LocalTimeZone);
+        }
+
+        [Fact]
+        public void MockingEvents()
+        {
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            var mockMailingRepository = new Mock<IMailingRepository>();
+            var customerService = new CustomerService(mockCustomerRepository.Object, mockMailingRepository.Object);
+
+            mockCustomerRepository.Raise(x => x.NotifySalesTeam += null, new NotifySalesTeamEventArgs(""));
+
+            mockMailingRepository.Verify(x => x.NewCustomerMessage(It.IsAny<string>()));
+        }
     }
 
     public interface ICustomerRepository
     {
+        string LocalTimeZone { get; set; }
+
+        Customer Customer { get; set; }
+        event EventHandler<NotifySalesTeamEventArgs> NotifySalesTeam;
+        IMailingRepository MailingRepository { get; set; }
+
         void Save(Customer customer);
+        void FetchAll();
     }
 
     public class CustomerService
     {
         private ICustomerRepository repo;
+        private IMailingRepository mailingRepository;
+        private ICustomerFullNameBuilder customerFullNameBuilder;
         private IIdFactory idFactory;
         private ICustomerAddressBuilder addressBuilder;
         private IMailingAddressFactory factory;
@@ -122,10 +214,36 @@ namespace Pluralsight.MockingWithMoq
             this.idFactory = idFactory;
         }
 
+        public CustomerService(ICustomerRepository repo, ICustomerFullNameBuilder customerFullNameBuilder)
+        {
+            this.repo = repo;
+            this.customerFullNameBuilder = customerFullNameBuilder;
+        }
+
+        public CustomerService(ICustomerRepository repo, IMailingRepository mailingRepository)
+        {
+            this.repo = repo;
+            this.mailingRepository = mailingRepository;
+
+            repo.NotifySalesTeam += NotifySalesTeam;
+        }
+        
+        public void NotifySalesTeam(object sender, NotifySalesTeamEventArgs e)
+        {
+            mailingRepository.NewCustomerMessage(e.Name);
+        }
+
         public void Create(CustomerToCreateDto dto)
         {
             var customer = new Customer(dto);
+
+            //Despite setting the value to Test2, the mock still will return the value specified in the Setup
+            //Unless the SetupProperty is used. In which case this will be properly get and set
+            repo.LocalTimeZone = "Test2";
+            var test = repo.LocalTimeZone;
+
             repo.Save(customer);
+            repo.FetchAll();
         }
 
         public void Create()
@@ -165,6 +283,39 @@ namespace Pluralsight.MockingWithMoq
                 repo.Save(new Customer(customerToCreate));
             }
         }
+
+        internal void CustomerFullNameBuilderTest(CustomerToCreateDto dto)
+        {
+            customerFullNameBuilder.From(dto.FirstName, dto.LastName);
+        }
+
+        internal void RecursiveMockingTest()
+        {
+            repo.MailingRepository.From(new CustomerToCreateDto());
+        }
+    }
+
+    public class NotifySalesTeamEventArgs : EventArgs
+    {
+        private string name;
+
+        public NotifySalesTeamEventArgs(string name)
+        {
+            this.name = name;
+        }
+
+        public object Name { get; internal set; }
+    }
+
+    public interface IMailingRepository
+    {
+        void From(CustomerToCreateDto customerToCreateDto);
+        void NewCustomerMessage(object name);
+    }
+
+    public interface ICustomerFullNameBuilder
+    {
+        void From(string firstName, string lastName);
     }
 
     public interface IIdFactory
@@ -174,6 +325,7 @@ namespace Pluralsight.MockingWithMoq
 
     public class MailingAddress
     {
+        public virtual string Address { get; set; }
     }
 
     public interface IMailingAddressFactory
@@ -189,10 +341,21 @@ namespace Pluralsight.MockingWithMoq
     public class CustomerToCreateDto
     {
         public int Id { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
     }
 
     public class Customer
     {
+        public virtual MailingAddress MailingAddress { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+
+        public Customer()
+        {
+
+        }
+
         public Customer(CustomerToCreateDto dto)
         {
 
